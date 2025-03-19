021b5386c5f567b8f630f8ce652d1b094a582ca4
mara0013 <mara0013@stud.kea.dk>
2025-03-19 11:09:33 +0100

    remove() funktion

---
+++ src/MainMenu.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainMenu {
+    Scanner scanner = new Scanner(System.in);
+
+    private ArrayList<String> orders;
+
+    public MainMenu() {
+        orders = new ArrayList<>();
+    }
+
+    public void addOrder(String order) {
+        orders.add(order);
+    }
+
+    public void removeOrder() {
+        System.out.println("Enter order number you wish to remove");
+        int orderIDInput = scanner.nextInt();
+        boolean found = false;
+
+        for (int i = 0; i < orders.size(); i++) {
+            if (orders.getOrdreID(i) == orderIDInput) {
+                orders.remove(i);
+                System.out.println("Order ID: " + orderIDInput + " has been removed from the list");
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+                System.out.println("Couldn't find order ID: " + orderIDInput + " on the list.");
+            }
+        }
+
+
+
+
+    }
+
+
+
+
+}
