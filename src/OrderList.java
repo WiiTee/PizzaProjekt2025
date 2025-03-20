d65ca637e553befe51dca909cfbcf0703c1bb755
WiiTee <somewhereinnowhere@live.dk>
2025-03-20 12:41:51 +0100

    compareTo metode til at sortere listen af bestillinger

--- src/OrderList.java
+++ src/OrderList.java
@@ -1,9 +1,16 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class OrderList {
     private ArrayList<Order> listOrder = new ArrayList<>();
 
+    public ArrayList<Order> getListOrder() {
+        return listOrder;
+    }
+
     public void prioritizedOrders(){
-        
+        listOrder.sort(Order::compareTo);
     }
 }
