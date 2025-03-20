9de63979d2f35a5b8f1bfe7a0aeca02202fc7edf
rasmaria <167328496+rasmaria@users.noreply.github.com>
2025-03-20 14:39:11 +0100

    Update OrderList.java

--- src/OrderList.java
+++ src/OrderList.java
@@ -2,9 +2,11 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class OrderList {
     private ArrayList<Order> listOrder = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
 
     public ArrayList<Order> getListOrder() {
         return listOrder;
@@ -13,4 +15,16 @@ public class OrderList {
     public void prioritizedOrders(){
         listOrder.sort(Order::compareTo);
     }
+
+    public void showOrderList(){
+        System.out.println("ORDER LIST:\n");
+        for (Order order : listOrder) {
+            System.out.println("Order ID: " + order.getOrderID());
+            System.out.println("Pick up time: " + order.getPickupTime());
+            System.out.println("(Order time: " + order.getOrderTime() + ")");
+            System.out.println("---------\n");
+        }
+    }
+
+
 }
